abcd510dbda9016c39ec2253389aa3106fde488c
andy <andy@andydote.co.uk>
2014-02-23 13:50:43 +0000

    bars: add stance bar

---
+++ bars/stanceBar.lua
@@ -0,0 +1,22 @@
+local addon, ns = ...
+
+local settings = ns.settings
+local bar = ns.bar
+
+bar:new({
+
+	name = "Stance",
+	bar = StanceBarFrame,
+	anchor = { "BOTTOMLEFT", "Left", "TOPLEFT", 0, settings.spacing },
+
+	rows = 1,
+	columns = NUM_STANCE_SLOTS,
+
+	init = function(self)
+
+		for i = 1, NUM_STANCE_SLOTS do
+			table.insert(self.buttons, _G["StanceButton" .. i])
+		end
+
+	end,
+})
